a746893bd2e5a71cb4ae9293bd3f178d344677ea
gligor99 <lukagligorevic@gmail.com>
2022-04-05 21:59:03 +0200

    Func for fetching done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Map from "./components/Map";
 import "./scss/main.scss";
 
 const App = () => {
+  const [input, setInput] = useState("");
+  const [ipData, setIpData] = useState([]);
+  const [url, setUrl] = useState("");
+
+  const handleClick = () => {
+    const res = input;
+    setUrl(res);
+  };
+
+  const API_KEY = `https://geo.ipify.org/api/v2/country?apiKey=at_cZE4uI1EdEKez7BfWmN8Qv0kzJciL&ipAddress=${url}`;
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    const newValue = e.target.value;
+    setInput(newValue);
+  };
+
+  const fetchIp = async (url) => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setIpData(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchIp(API_KEY);
+  }, [url]);
+
   return (
     <section className="app-container">
-      <Header />
+      <Header
+        ipData={ipData}
+        handleClick={handleClick}
+        handleChange={handleChange}
+      />
       <Map />
     </section>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import arrowIcon from "../images/icon-arrow.svg";
 
-const Header = () => {
+const Header = ({ handleClick, ipData, handleChange }) => {
   return (
     <section className="header">
       <h1 className="header__title">IP Address Tracker</h1>
@@ -10,31 +10,28 @@ const Header = () => {
           className="header__input"
           type="text"
           placeholder="Search for any IP address or domain"
+          onChange={handleChange}
         />
-        <button className="header__input-btn">
+        <button className="header__input-btn" onClick={handleClick}>
           <img src={arrowIcon} alt="Button Arrow Icon" />
         </button>
       </div>
       <div className="header__details-section">
         <div className="header__details-section__item">
           <h5>ip address</h5>
-          <h2>192.168.0.1</h2>
+          <h2>{ipData.ip}</h2>
         </div>
         <div className="header__details-section__item">
           <h5>Location</h5>
-          <h2>
-            Brooklyn, NY, <br /> 10001
-          </h2>
+          <h2>{ipData.location?.region}</h2>
         </div>
         <div className="header__details-section__item">
           <h5>timezone</h5>
-          <h2>UTC -05:00</h2>
+          <h2>{ipData.location?.timezone}</h2>
         </div>
         <div className="header__details-section__item">
           <h5>isp</h5>
-          <h2>
-            SpaceX <br /> Starlink
-          </h2>
+          <h2>{ipData.isp}</h2>
         </div>
       </div>
     </section>
