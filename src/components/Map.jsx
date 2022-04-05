7170f519654ff4110683af9c241cafb1bc052f7b
gligor99 <lukagligorevic@gmail.com>
2022-04-05 23:30:40 +0200

    Leaflet Added

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { MapContainer, Popup, TileLayer, Marker } from "react-leaflet";
 
-// pk.eyJ1IjoibHVrYWc5OSIsImEiOiJja3MxdnRiYXcxeTl4Mm5xampiN2MwNmNmIn0.G2PXR-h5-EE3zFLZBNggRg
 const MapComponent = () => {
-  return <section className="map"></section>;
+  return (
+    <section className="map">
+      <MapContainer className="map__container" center={[51.505, -0.09]} zoom={13}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </section>
+  );
 };
 
 export default MapComponent;
