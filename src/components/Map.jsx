e375c8f9ee239c30ce6394c605a24d497dcb1613
gligor99 <lukagligorevic@gmail.com>
2022-04-07 00:20:37 +0200

    Responsive

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React from "react";
 import { MapContainer, Popup, TileLayer, Marker } from "react-leaflet";
 import { PuffLoader } from "react-spinners";
 
